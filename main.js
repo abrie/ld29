afd2e4a56d3e49128bc7d92c39cc58ac61b8e984
abrie <abrie@>
2014-04-26 05:28:42 +0100

    add Tile class

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
 "use strict";
 require(['lib/three.min'], function() {
         
-    var WIDTH = 10;
-    var HEIGHT = 10;
+    var scene = new THREE.Scene();
+
+    var WIDTH = 5;
+    var HEIGHT = 5;
     var TileColors = [0xff0000,0x00ff00,0x0000ff]; 
+
     function randomFromArray( array ) {
         return array[ Math.floor( Math.random()*array.length) ];
     }
 
-    var meshes = [];
-    for( var x = 0; x < WIDTH; x++ ) {
-        for( var y = 0; y < HEIGHT; y++ ) {
-            var geometry = new THREE.PlaneGeometry(1/WIDTH,1/HEIGHT);
-            var material = new THREE.MeshBasicMaterial( { color: randomFromArray(TileColors), wireframe: false } );
-            var mesh = new THREE.Mesh( geometry, material );
-            mesh.position.x = 1/WIDTH*x - 0.5 + 1/WIDTH/2;
-            mesh.position.y = 1/HEIGHT*y - 0.5 + 1/HEIGHT/2;
-            mesh.position.z = 0.005;
-            meshes.push(mesh);
+    function localToModel(x,y,z) {
+        return new THREE.Vector3(
+            -0.5+1/WIDTH*x+1/WIDTH/2,
+            -0.5+1/HEIGHT*y+1/WIDTH/2,
+            z
+        ); 
+    }
+
+    var Tile = function(x,y,z) {
+        var geometry = new THREE.PlaneGeometry(1/WIDTH,1/HEIGHT);
+        var material = new THREE.MeshBasicMaterial( { color: randomFromArray(TileColors), wireframe: false } );
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.position = localToModel(x, y, 0.005);
+
+        return {
+            x:x,
+            y:y,
+            mesh:mesh
         }
     }
+
+    var tiles = [];
+    for( var x = 0; x < WIDTH; x++ ) {
+        for( var y = 0; y < HEIGHT; y++ ) {
+            var tile = new Tile(x,y,0.005)
+            tiles.push(tile);
+        }
+    }
+
+    var heli_geometry = new THREE.PlaneGeometry(1/WIDTH,1/HEIGHT);
+    var heli_material = new THREE.MeshBasicMaterial( { color: 0xFF00FF, wireframe: false } );
+    var heli_mesh = new THREE.Mesh( heli_geometry, heli_material );
+    heli_mesh.rotation.x = Math.PI/2;
+    heli_mesh.position = localToModel(0,0,0.1);
+    scene.add( heli_mesh );
 
     var camera = new THREE.PerspectiveCamera( 15, window.innerWidth / window.innerHeight, 1, 10000 );
     var cameraCenter = {x:0, y:-3, z:2};
@@ -32,10 +58,10 @@ require(['lib/three.min'], function() {
     var material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: false } );
     var mesh = new THREE.Mesh( geometry, material );
 
-    var scene = new THREE.Scene();
     scene.add( mesh );
-    meshes.forEach( function(m) {
-        scene.add( m );
+
+    tiles.forEach( function( tile ) {
+        scene.add( tile.mesh );
     })
 
     var renderer = new THREE.WebGLRenderer();
