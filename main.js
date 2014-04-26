49c4fe0642d42a0bd8b76a4d4df3b12ef44afe23
abrie <abrie@>
2014-04-26 06:53:58 +0100

    add a rotor to the helicopter

--- main.js
+++ main.js
@@ -10,20 +10,20 @@ require(['util','lib/three.min'], function(util) {
 
     var scene = new THREE.Scene();
 
-    var WIDTH = 5;
-    var HEIGHT = 5;
+    var MAP_WIDTH = 5;
+    var MAP_HEIGHT = 5;
     var TileColors = [0xff0000,0x00ff00,0x0000ff]; 
 
     function localToModel(x,y,z) {
         return new THREE.Vector3(
-            -0.5+1/WIDTH*x+1/WIDTH/2,
-            -0.5+1/HEIGHT*y+1/WIDTH/2,
+            -0.5+1/MAP_WIDTH*x+1/MAP_WIDTH/2,
+            -0.5+1/MAP_HEIGHT*y+1/MAP_WIDTH/2,
             z
         ); 
     }
 
     var Tile = function(x,y,z) {
-        var geometry = new THREE.PlaneGeometry(1/WIDTH,1/HEIGHT);
+        var geometry = new THREE.PlaneGeometry(1/MAP_WIDTH,1/MAP_HEIGHT);
         var material = new THREE.MeshBasicMaterial( { map: textures.grass, wireframe: false } );
         var mesh = new THREE.Mesh( geometry, material );
         mesh.position = localToModel(x, y, 0.005);
@@ -37,8 +37,8 @@ require(['util','lib/three.min'], function(util) {
     }
 
     var tiles = [];
-    for( var x = 0; x < WIDTH; x++ ) {
-        for( var y = 0; y < HEIGHT; y++ ) {
+    for( var x = 0; x < MAP_WIDTH; x++ ) {
+        for( var y = 0; y < MAP_HEIGHT; y++ ) {
             var tile = new Tile(x,y,0.005)
             tiles.push(tile);
         }
@@ -68,7 +68,13 @@ require(['util','lib/three.min'], function(util) {
 
     linkTwoTiles();
 
-    var heli_geometry = new THREE.PlaneGeometry(1/WIDTH,1/HEIGHT);
+    var rotor_geometry = new THREE.PlaneGeometry(1/MAP_WIDTH,1/MAP_HEIGHT/15);
+    var rotor_material = new THREE.MeshBasicMaterial( {color:0xFF00FF, wireframe: false} ); 
+    var rotor_mesh = new THREE.Mesh( rotor_geometry, rotor_material );
+    rotor_mesh.position = localToModel(0,0,0.2);
+    scene.add( rotor_mesh );
+
+    var heli_geometry = new THREE.PlaneGeometry(1/MAP_WIDTH,1/MAP_HEIGHT);
     var heli_material = new THREE.MeshBasicMaterial( { map: textures.heli, transparent:true, wireframe: false } );
     var heli_mesh = new THREE.Mesh( heli_geometry, heli_material );
     heli_mesh.rotation.x = Math.PI/2;
@@ -120,6 +126,8 @@ require(['util','lib/three.min'], function(util) {
         camera.position.z = Math.cos(theta)*0.05 + cameraCenter.z;
         camera.position.y = Math.cos(theta)*0.005 + cameraCenter.y;
         camera.position.x = Math.cos(theta)*0.005 + cameraCenter.x;
+
+        rotor_mesh.rotation.z += 1;
 
         renderer.render( scene, camera );
     }
